200f050495d55bc82f350d49c6f20bf62c72fa5e
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-03-14 21:41:23 +0530

    main

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -7,7 +7,7 @@ function Signup() {
     <>
     <div className="login">
         <h2>Signup</h2>
-        <p>Already a member?<Link to='/login' className='links'> Login.</Link></p>
+        <p>Already a member?<Link to='/login' className='links'>  Login.</Link></p>
         <label>Email</label><br></br>
         <input placeholder='Enter email'></input><br></br>
         <label>Password</label><br></br>
